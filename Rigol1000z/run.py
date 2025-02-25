8d93b1e770602fba1813356069b262c179f7cf3c
Mathias Kornschober <mathias.kornschober@bulme.at>
2025-02-25 18:40:30 +0100

    Changed run.py to not always save to <channels.csv>, but into a folder

--- Rigol1000z/run.py
+++ Rigol1000z/run.py
@@ -2,6 +2,7 @@ import pyvisa as visa
 from Rigol1000z import Rigol1000z
 from Rigol1000z.constants import *
 from time import sleep
+import os
 
 # https://github.com/AlexZettler/Rigol1000z
 
@@ -15,9 +16,27 @@ instrument = rm.open_resource("TCPIP0::172.16.62.104::INSTR")
 print(f"Using " + instrument.query("*IDN?") )
 
 
-with Rigol1000z(instrument) as oszi:
+def get_next_filename(folder: str, prefix: str, extension: str) -> str:
+    os.makedirs(folder, exist_ok=True)
+    counter_file = os.path.join(folder, "counter.txt")
+    
+    # Load or initialize counter
+    if os.path.exists(counter_file):
+        with open(counter_file, "r") as f:
+            counter = int(f.read().strip())
+    else:
+        counter = 1
+    
+    filename = os.path.join(folder, f"{prefix}{counter:03d}.{extension}")
+    
+    # Increment and save counter
+    with open(counter_file, "w") as f:
+        f.write(str(counter + 1))
+    
+    return filename
 
-    #### README: reset und autoscale nut EINMAL nötig, danach auskommentieren
+with Rigol1000z(instrument) as oszi:
+    #### README: reset und autoscale nur EINMAL nötig, danach auskommentieren
     #oszi.ieee488.reset()
     #oszi.autoscale()
     ####
@@ -28,15 +47,9 @@ with Rigol1000z(instrument) as oszi:
     for ch in range(1, 2):
         oszi[ch].enabled = True  # Enable the channel
 
-    # Set the horizontal timebase
-    #oszi.timebase.mode = ETimebaseMode.Main  # Set the timebase mode to main (normal operation)
-    #oszi.timebase.scale = 10E-6  # Set the timebase scale in seconds / div
-        
-    # we can set the y axis scale in V / div
-    #oszi[1].scale_v = 2
-
     # stop before getting data
     oszi.stop()
     
-    #oszi.get_screenshot('./screenshot.png') 
-    oszi.get_data(EWaveformMode.Raw, './channels.csv')  # Collect and save waveform data from all enabled channels
+    # Generate unique filename
+    filename = get_next_filename("channels", "channels", "csv")
+    oszi.get_data(EWaveformMode.Raw, filename)  # Collect and save waveform data
